52109f53f1c5b019d99aa63fbf1dfc778cb3b9c8
Andreas Fäctare <andreasfactare@gmail.com>
2022-08-24 20:56:02 +0200

    edited cors

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const server = http.createServer(app);
 
 const io = socketIO(server, {
   cors: {
-    origin: "https://faectarn-chat-frontend.herokuapp.com/",
+    origin: "*",
     methods: ["GET", "POST", "DELETE"]
   }
 });
