2109f526b2c2f5090b9482bfe21c79e276e45b19
Andreas Fäctare <andreasfactare@gmail.com>
2022-08-26 13:04:56 +0200

    edit message

--- models/messages.model.js
+++ models/messages.model.js
@@ -1,6 +1,6 @@
 const DB = require("../database");
 
-async function addMessage(message, username, user_id, room_name, time) {
+async function addMessage({message, username, user_id, room_name, time}) {
   const SQL =
     "INSERT INTO messages (message, username, user_id, room_name, time) VALUES ($1, $2, $3, $4, $5)";
   const result = await DB.query(SQL, [message, username, user_id, room_name, time]);
